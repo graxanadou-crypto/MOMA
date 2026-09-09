f60afd7863a840670351e3022415ed5ca5846795
graxanadou-crypto <graxanadou@gmail.com>
2026-09-10 06:13:12 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='moma-solitaire-v1.0.1';
-const ASSETS=["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./assets/cards/ancient-tomb.jpg", "./assets/cards/city-of-brass.jpg", "./assets/cards/tolarian-academy.jpg", "./assets/cards/tundra.jpg", "./assets/cards/volcanic-island.jpg", "./assets/cards/lotus-petal.jpg", "./assets/cards/mana-vault.jpg", "./assets/cards/mox-diamond.jpg", "./assets/cards/scroll-rack.jpg", "./assets/cards/voltaic-key.jpg", "./assets/cards/mind-over-matter.jpg", "./assets/cards/abeyance.jpg", "./assets/cards/intuition.jpg", "./assets/cards/power-sink.jpg", "./assets/cards/stroke-of-genius.jpg", "./assets/cards/time-spiral.jpg", "./assets/cards/windfall.jpg", "./assets/cards/card-back.jpg"];
+const CACHE='moma-solitaire-v1.0.4';
+const ASSETS=["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./ancient-tomb.jpg", "./city-of-brass.jpg", "./tolarian-academy.jpg", "./tundra.jpg", "./volcanic-island.jpg", "./lotus-petal.jpg", "./mana-vault.jpg", "./mox-diamond.jpg", "./scroll-rack.jpg", "./voltaic-key.jpg", "./mind-over-matter.jpg", "./abeyance.jpg", "./intuition.jpg", "./power-sink.jpg", "./stroke-of-genius.jpg", "./time-spiral.jpg", "./windfall.jpg", "./card-back.jpg"];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
