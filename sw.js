b6d18164182042d0ad133e598a46a7c91b3bd5d4
graxanadou-crypto <graxanadou@gmail.com>
2026-09-10 23:59:10 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,5 @@
-const CACHE='moma-solitaire-v1.3-game-ui';
+const CACHE='moma-solitaire-v1.11-current-mulligan';
 const ASSETS=["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./abeyance.jpg", "./ancient-tomb.jpg", "./card-back.jpg", "./city-of-brass.jpg", "./intuition.jpg", "./lotus-petal.jpg", "./mana-vault.jpg", "./mind-over-matter.jpg", "./mox-diamond.jpg", "./power-sink.jpg", "./scroll-rack.jpg", "./stroke-of-genius.jpg", "./time-spiral.jpg", "./tolarian-academy.jpg", "./tundra.jpg", "./volcanic-island.jpg", "./voltaic-key.jpg", "./windfall.jpg"];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
-  const u=new URL(e.request.url);
-  if(u.origin!==self.location.origin) return;
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
-    const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return resp;
-  })));
-});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin!==self.location.origin)return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;})));});
